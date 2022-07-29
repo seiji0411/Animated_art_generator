16dd131f3a9b8201fe6b271dc2ef28031564148b
Seiji Ito <seijiito1102@outlook.com>
2022-07-29 22:28:17 +0800

    add the sciprt for uploading to ipfs

---
+++ upload_to_ipfs/config.ts
@@ -0,0 +1,2 @@
+export const infura_ipfs_key = "25Hxk1M18paxckqd9rUuCCIDbp1";
+export const infura_ipfs_secret = "d02ba1c3ae1f1c8758d1e56cb3df6805";
---
+++ upload_to_ipfs/upload_json.ts
@@ -0,0 +1,64 @@
+import fs from "fs";
+import path from "path";
+import {create, IPFSHTTPClient} from "ipfs-http-client";
+// @ts-ignore
+import { infura_ipfs_key, infura_ipfs_secret } from "./config.ts";
+
+let ipfs: IPFSHTTPClient;
+const tokenIpfs = `${infura_ipfs_key}:${infura_ipfs_secret}`;
+const authIpfs = Buffer.from(tokenIpfs).toString('base64');
+try {
+    ipfs = create({
+        url: "https://ipfs.infura.io:5001/api/v0",
+        headers: {
+            Authorization: `Basic ${authIpfs}`,
+        },
+        timeout: 600000,
+    });
+} catch (error) {
+    console.error("IPFS error ", error);
+    throw new Error("loading IPFS was failed");
+}
+
+const main = async () => {
+    console.log("\n\n Loading json files...\n");
+    const imagesFolder = "../build/json/";
+
+    const files = fs.readdirSync(imagesFolder);
+
+    const params = [];
+    for (const index in files) {
+        const filename = files[index].replace('.json', '');
+        const filePath = path.join(imagesFolder, files[index]);
+        const stat = fs.lstatSync(filePath);
+
+        if (!stat.isDirectory() && filename !== ".DS_Store") {
+            const json = fs.readFileSync(filePath);
+            params.push({ path: filename, content: json });
+        }
+    }
+    const uploaded = await ipfs.addAll(params, { wrapWithDirectory: true, pin: true });
+
+    let cid: string = "";
+    try {
+        for await (const file of uploaded) {
+            console.log("  Uploaded: ", file);
+            if (file.path.length === 0) {
+                cid = file.cid.toString();
+            }
+        }
+    } catch (err) {
+        console.error(err);
+    }
+
+    console.log(
+        `NFT Metadata uploaded. Directory CID: ${cid}, Number: ${params.length}, Directory Path: https://ipfs.io/ipfs/${cid}/`
+    );
+};
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        throw new Error("Excuting failed");
+    });
